3fdf25c389054e81bfe9f8a0c3a08d9b2e6008c6
Nikolay <rachkuw@gmail.com>
2021-03-29 19:25:43 +0300

    Adding background

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Simple card</title>
+</head>
+
+<body>
+<img class="img-top" src="./assets/images/bg-pattern-top.svg">
+<img class="img-bottom" src="./assets/images/bg-pattern-bottom.svg">
+</body>
+
+</html>
