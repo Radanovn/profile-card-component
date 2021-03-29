426ad769940cc50ace8db0e3550cfed8abe2b311
Nikolay <rachkuw@gmail.com>
2021-03-29 20:29:30 +0300

    Added card

--- index.php
+++ index.php
@@ -4,10 +4,29 @@
     <link rel="stylesheet" href="assets/css/styles.css">
     <title>Simple card</title>
 </head>
-
 <body>
-<img class="img-top" src="./assets/images/bg-pattern-top.svg">
-<img class="img-bottom" src="./assets/images/bg-pattern-bottom.svg">
-</body>
+    <img class="img-top" src="./assets/images/bg-pattern-top.svg">
+    <img class="img-bottom" src="./assets/images/bg-pattern-bottom.svg">
 
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+
+            </div>
+            <img src="./assets/images/image-victor.jpg">
+
+            <div class="text">
+                <h4><b>Victor Crest</b> <span class="muted">26</span></h4>
+                <small class="muted">London</small>
+
+                <hr>
+                <div class="socials">
+                    <p>80K<span class="muted"><br>Followers</span></p>
+                    <p>803K <span class="muted"><br>Likes</span></p>
+                    <p>1.4K<span class="muted"><br>Photos</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
